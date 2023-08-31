e292ed45190dcef9266e07f10693ea3b68b8fd59
azh-1415926 <AzhandHJ@outlook.com>
2023-08-31 10:33:51 +0800

    将 glad.c 挪到工程中

--- LearnOpenGL/main.cpp
+++ LearnOpenGL/main.cpp
@@ -3,14 +3,33 @@
 
 #include <iostream>
 
+/* 调整视口的回调函数 */
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+    /* 告诉OpenGL渲染窗口的尺寸大小 */
+    glViewport(0, 0, width, height);
+}
+
+/* 当按下 Esc 键时请求退出窗口 */
+void processInput(GLFWwindow* window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+}
+
 int main()
 {
+    /* 初始化 GLFW */
     glfwInit();
+    /* 配置 GLFW，使用 3.3 版本的 OpenGL，主版本号(Major)和次版本号(Minor)都设置为 3 */
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    /* 使用核心模式(Core-profile) */
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    /* Mac 系统需要取消下一行的注释 */
     //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
+    /* 创建窗口 */
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
     if (window == NULL)
     {
@@ -18,7 +37,37 @@ int main()
         glfwTerminate();
         return -1;
     }
+    /* 通知GLFW将 window 的上下文设置为当前线程的主上下文 */
     glfwMakeContextCurrent(window);
 
+    /* 初始化 GLAD */
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    }
+
+    /* 注册视口变化回调函数 */
+    glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+    
+    /* 循环渲染，glfwWindowShouldClose 、检查 GLFW 是否被要求退出 */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* 设置用于清屏的颜色、清屏 */
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* 输入 */
+        processInput(window);
+
+        /* 渲染 ... */
+
+        /* 监听事件、交换缓冲 */
+        glfwPollEvents();
+        glfwSwapBuffers(window);
+    }
+
+    /* 释放资源 */
+    glfwTerminate();
     return 0;
 }
